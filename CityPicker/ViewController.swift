b87df2fec47bf53cf8560900ac89f061ad1636b7
龚杰洪 <gongjh@sudyapp.com>
2018-09-28 14:56:30 +0800

    Initial Commit

---
+++ CityPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityPicker
+//
+//  Created by 龚杰洪 on 2018/9/28.
+//  Copyright © 2018年 龚杰洪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
